7a234e58aead8e913b80a64af4115a12fe2103af
Leon Aves <leonaves1@gmail.com>
2016-03-18 03:12:19 +0000

    okay I went too far with that last one.

--- src/spotifyAPI.js
+++ src/spotifyAPI.js
@@ -6,11 +6,15 @@ var SpotifyAPI = function (baseUrl) {
     this.baseUrl = baseUrl || 'https://api.spotify.com/v1/';
 };
 
-SpotifyAPI.prototype.handleError = error => console.log("error with Spotify web API request: " + error);
+SpotifyAPI.prototype.handleError = function (err) {
+    console.log("error with Spotify web API request: " + err);
+};
 
-SpotifyAPI.prototype.getUrl = action => encodeURI(this.baseUrl + action);
+SpotifyAPI.prototype.getUrl = function (action) {
+    return encodeURI(this.baseUrl + action);
+};
 
-SpotifyAPI.prototype.search = query => {
+SpotifyAPI.prototype.search = function(query) {
     let searchURI = this.getUrl('search?q=' + query + '&type=track&limit=10');
 
     return request(searchURI)
